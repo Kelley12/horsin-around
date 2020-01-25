6943779302b43526ffd4ee3b9894d81e0afa0af3
Blake <blakekelley127@gmail.com>
2020-01-25 13:57:06 -0700

    Adding additional entity validation

--- src/server/entity/user.ts
+++ src/server/entity/user.ts
@@ -1,22 +1,29 @@
 import {
-    Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BeforeInsert
+    Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Unique
 } from "typeorm";
 import { IUser } from "../../shared";
 import bcrypt from "bcryptjs";
+import { Length, IsEmail } from "class-validator";
 
 @Entity("users")
+@Unique(["email"])
 export class User implements IUser {
     @PrimaryGeneratedColumn()
     public userId?: number;
 
     @Column("varchar", { length: 50, unique: true })
+    @IsEmail()
     public email!: string;
 
     @Column("varchar", { length: 50 })
     public name!: string;
 
     @Column("varchar", { length: 60 })
+    @Length(8, 60)
     public password!: string;
+
+    @Column("varchar", { default: "user" })
+    public role!: string;
 
     @CreateDateColumn({ type: "timestamp with time zone" })
     public createDate!: Date;
