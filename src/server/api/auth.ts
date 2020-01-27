03214c695b02bec860ffbed3fd00c299388a5a38
Blake <blakekelley127@gmail.com>
2020-01-26 20:57:37 -0700

    Adding API authorization

--- src/server/api/auth.ts
+++ src/server/api/auth.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { AuthController } from "../controllers";
+import { authOwnedUserReq } from "../middleware";
 
 export function authRouter(): express.Router {
     const router = express.Router();
 
     router.post("/login", AuthController.login);
     router.post("/session-is-valid", AuthController.validateSession);
-    router.post("/change-password", AuthController.changePassword);
+    router.post("/change-password", [authOwnedUserReq], AuthController.changePassword);
 
     return router;
 }
--- src/server/api/user.ts
+++ src/server/api/user.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import { userController } from "../controllers";
+import { authRole, authOwnedUserReq } from "../middleware";
 
 export function userRouter(): express.Router {
     const router = express.Router();
 
-    router.get("/", userController.getUsers);
-    router.get("/:id", userController.getUser);
-    router.post("/", userController.createUser);
-    router.put("/:id", userController.updateUser);
-    router.delete("/:id", userController.deleteUser);
+    router.get("/", [authRole], userController.getUsers);
+    router.get("/:id", [authOwnedUserReq], userController.getUser);
+    router.post("/", [authRole], userController.createUser);
+    router.put("/:id", [authOwnedUserReq], userController.updateUser);
+    router.delete("/:id", [authOwnedUserReq], userController.deleteUser);
 
     return router;
 }
