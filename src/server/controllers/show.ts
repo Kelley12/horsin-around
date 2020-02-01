286abf079aad16d8dbe1dd86fec85853703b36b7
Blake <blakekelley127@gmail.com>
2020-01-31 19:51:55 -0700

    Convert show controller to handle request and response

--- src/server/controllers/show.ts
+++ src/server/controllers/show.ts
@@ -1,36 +1,116 @@
 import { EventEmitter2 } from "eventemitter2";
+import { Request, Response } from "express";
+import { validate } from "class-validator";
+import { logger } from "../utils";
+import { getRepository } from "typeorm";
 import { Show } from "../entity";
-import { getManager, Repository } from "typeorm";
 
 export class ShowController {
     private readonly emitter = new EventEmitter2();
-    private readonly repository: Repository<Show>;
 
-    constructor() {
-        this.repository = getManager().getRepository(Show);
+    async getShows(_: Request, res: Response) {
+        try {
+            const showRepository = getRepository(Show);
+            const shows = await showRepository.find();
+
+            res.send(shows);
+        } catch (error) {
+            logger.log("error", `API Error:`);
+            logger.log("error", error);
+            res.status(500).json({ error });
+        }
     }
 
-    getShows(): Promise<Show[]> {
-        return this.repository.find();
+    async getShow(req: Request, res: Response) {
+        try {
+            const id = parseInt(req.params.id);
+            const showRepository = getRepository(Show);
+            const show = await showRepository.findOneOrFail(id);
+            res.send(show);
+        } catch (error) {
+            res.status(404).send("Show not found");
+        }
     }
 
-    getShow(id: number): Promise<Show | undefined> {
-        return this.repository.findOne(id);
+    async createShow(req: Request, res: Response) {
+        const { name, showDate } = req.body;
+
+        if (!name || !showDate) {
+            return res.status(400)
+                .send({ error: "Missing data: name or showDate" });
+        }
+
+        const show = new Show();
+        show.name = name;
+        show.showDate = showDate;
+
+        const errors = await validate(show);
+        if (errors.length > 0) {
+            res.status(400).send(errors);
+            return;
+        }
+
+        try {
+            const showRepository = getRepository(Show);
+            await showRepository.save(show);
+        } catch (e) {
+            res.status(409).send("Invalid show");
+            return;
+        }
+
+        res.status(201).send(show);
     }
 
-    createShow(show: Show): Promise<Show> {
-        const newShow = this.repository.create(show);
-        return this.repository.save(newShow);
+    async updateShow(req: Request, res: Response) {
+        const id = parseInt(req.params.id);
+        const { name, showDate } = req.body;
+
+        if (!name || !showDate) {
+            return res.status(400)
+                .send({ error: "Missing data: name or showDate" });
+        }
+
+        let show;
+        const showRepository = getRepository(Show);
+        try {
+            show = await showRepository.findOneOrFail(id);
+        } catch (error) {
+            res.status(404).send("Show not found");
+            return;
+        }
+
+        show.name = name;
+        show.showDate = showDate;
+
+        const errors = await validate(show);
+        if (errors.length > 0) {
+            res.status(400).send(errors);
+            return;
+        }
+
+        try {
+            await showRepository.save(show);
+        } catch (e) {
+            res.status(409).send("Invalid show");
+            return;
+        }
+
+        res.status(200).send(show);
     }
 
-    async updateShow(show: Show, id: number): Promise<Show | undefined> {
-        await this.repository.update(id, show);
-        return this.repository.findOne(id);
-    }
+    async deleteShow(req: Request, res: Response) {
+        const id = parseInt(req.params.id);
 
-    async deleteShow(id: number): Promise<boolean> {
-        const deleted = await this.repository.delete(id);
-        return deleted.raw[1] ? true : false;
+        const showRepository = getRepository(Show);
+        try {
+            await showRepository.findOneOrFail(id);
+        } catch (error) {
+            res.status(404).send("Show not found");
+            return;
+        }
+        showRepository.delete(id);
+
+        res.status(200).send(true);
     }
 
     on(event: "Error", cb: (error: Error) => void): this;
@@ -49,3 +129,5 @@ export class ShowController {
         return this;
     }
 }
+
+export const showController = new ShowController();
