b331a1cd6dddedcd40e18d80fc5dc03da3dafe3b
Blake <blakekelley127@gmail.com>
2020-02-02 09:30:56 -0700

    Adding Show interface and emptyShow

--- src/shared/interfaces/show.ts
+++ src/shared/interfaces/show.ts
@@ -2,3 +2,15 @@ export interface IShow {
     name: string;
     showDate: Date;
 }
+
+export interface Show {
+    showId: number;
+    name: string;
+    showDate: Date;
+}
+
+export const emptyShow: Show = {
+    showId: 0,
+    name: "",
+    showDate: new Date()
+};
