777118f6076ef2aa43bd1987cdc7aeacb345a497
Blake <blakekelley127@gmail.com>
2020-02-06 18:09:02 -0700

    Adding Result interface and empty result

--- src/shared/interfaces/result.ts
+++ src/shared/interfaces/result.ts
@@ -9,3 +9,27 @@ export interface IResults {
     seconds: number;
     milliseconds: number;
 }
+
+export interface Result {
+    resultId: number;
+    showClassId: number;
+    riderId: number;
+    horse: string;
+    scored: boolean;
+    faults: number;
+    minutes: number;
+    seconds: number;
+    milliseconds: number;
+}
+
+export const emptyResult: Result = {
+    resultId: 0,
+    showClassId: 0,
+    riderId: 0,
+    horse: "",
+    scored: false,
+    faults: 0,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0
+};
