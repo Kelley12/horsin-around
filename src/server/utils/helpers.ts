b0aa85ae3f2e98aec3a7c0d1ccdcc1f37521ccf1
Blake <blakekelley127@gmail.com>
2020-02-09 18:24:11 -0700

    Fixing placing algorithm

--- src/server/utils/helpers.ts
+++ src/server/utils/helpers.ts
@@ -4,28 +4,35 @@ import { Result } from "../entity";
 type Wrapper = ((router: Router) => void);
 
 export const applyMiddleware = (
-  middleware: Wrapper[],
-  router: Router
+    middleware: Wrapper[],
+    router: Router
 ) => {
-  for (const apply of middleware) {
-    apply(router);
-  }
+    for (const apply of middleware) {
+        apply(router);
+    }
 };
 
 export function sortByTimeDiff(array: Result[], optimumTime: number) {
-  return array.sort((a, b) => {
-      const elimination = compareElimination(a, b);
-      if (elimination !== 0) return elimination;
+    return array.sort((a, b) => {
+        const elimination = compareElimination(a, b);
+        if (elimination !== 0) return elimination;
 
-      const x = Math.abs(a.timeInMs - optimumTime) + (a.faults * 1000) + (a.timePenalty * 1000);
-      const y = Math.abs(b.timeInMs - optimumTime) + (b.faults * 1000) + (b.timePenalty * 1000);
+        const x = Math.abs(a.timeInMs - optimumTime) + // Time Diff
+            Math.max(0, a.timeInMs - optimumTime) + // Penalty for over opt time
+            (a.faults * 1000) + // Fault penalty
+            (a.timePenalty * 1000); // Additional time penalty
 
-      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-  });
+        const y = Math.abs(b.timeInMs - optimumTime) + // Time Diff
+            Math.max(0, b.timeInMs - optimumTime) + // Penalty for over opt time
+            (b.faults * 1000) + // Fault penalty
+            (b.timePenalty * 1000); // Additional time penalty
+
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
 }
 
 export function compareElimination(resultA: Result, resultB: Result) {
-  if (resultA.eliminated && !resultB.eliminated) return 1;
-  if (!resultA.eliminated && resultB.eliminated) return -1;
-  return 0;
+    if (resultA.eliminated && !resultB.eliminated) return 1;
+    if (!resultA.eliminated && resultB.eliminated) return -1;
+    return 0;
 }
