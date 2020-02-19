98c3a623a8c545b977ea4b941eebee75f4f1af20
Blake <blakekelley127@gmail.com>
2020-02-18 22:14:29 -0700

    Fixing login

--- src/client/components/login/index.ts
+++ src/client/components/login/index.ts
@@ -1,14 +1,13 @@
 import Vue from "vue";
-import { state, checkForSession } from "../../state";
+import { state } from "../../state";
 import { post, apiurl } from "../../helpers";
-import { User } from "../../../shared";
 
 export const LoginBox = Vue.extend({
     template: require("./login.html"),
     data() {
         return {
             ...state.get(),
-            username: "",
+            email: "",
             password: "",
             error: "",
             loading: false,
@@ -24,16 +23,14 @@ export const LoginBox = Vue.extend({
             this.error = "";
             if (this.loading) return;
             this.loading = true;
-            post(`${apiurl}/login`, {
-                id: this.username,
+            post(`${apiurl}/auth/login`, {
+                email: this.email,
                 password: this.password
             })
-                .then(({ token, user }: { token: string, user: User }) => {
-                    localStorage.setItem("session", JSON.stringify({ token, user }));
-                    checkForSession();
+                .then(() => {
                     this.$router.push("/");
                 })
-                .catch((e: Error) => this.error = e.message)
+                .catch((e: Error) => this.error = e.message )
                 .then(() => this.loading = false);
         }
     }
